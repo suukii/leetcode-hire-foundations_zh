99810698d9b5950580cc44c8f1df52953bb4e152
suki <suki_yueng@163.com>
2020-07-05 14:12:04 +0800

    feat: parseError

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+interface StackMessage {
+    line: number;
+    column: number;
+    filename: string;
+}
+
+export interface ErrorMessage {
+    message: string;
+    stack: Array<StackMessage>;
+}
+
+export function parseError(err: Error): ErrorMessage {
+    const stack: Array<string> = (err.stack || '').split('\n').slice(1);
+
+    const parsedStack = stack
+        .map(
+            (msg: string): StackMessage => {
+                const detail = /([^\s|@]+\.[^\.]+)?:(\d+):(\d+)$/.exec(msg) || [];
+                const [, filename, line, column] = detail;
+                return {
+                    line: Number(line),
+                    column: Number(column),
+                    filename: filename || '',
+                };
+            },
+        )
+        .filter((detail: StackMessage): boolean => detail.filename !== '');
+
+    return {
+        message: err.message,
+        stack: parsedStack,
+    };
+}
